b451c7e0905ca058de4a80d63ae7e68e107cadf2
Daniel Rock <rockdaniel87@yahoo.com>
2019-02-27 02:57:49 -0500

    Changed funtionality with use of eval method

--- js/modules/calc.js
+++ js/modules/calc.js
@@ -1,10 +1,7 @@
 const Calculate = (_ => {
   let numArray = [];
-  let addedNumbersArray = [];
-  let minusNumbersArray = [];
-  let timesNumbersArray = [];
-  let divideNumbersArray = [];
-  let finalNumber = 0;
+  let pendingNumbersArray = [];
+  let equals;
 
   const $calculator = document.querySelector(".calculator");
   const $total = document.querySelector(".calculator__total");
@@ -23,61 +20,41 @@ const Calculate = (_ => {
 
   const checkOperator = symbol => {
     if (symbol === "+") {
-      addedNumbersArray.push(numberArrayStorage(numArray));
+      pendingNumbersArray.push(numArray.join(''));
+      pendingNumbersArray.push('+');
       numArray.length = 0;
-      $total.innerHTML = "";
+      $total.innerHTML = "0";
     }
     if (symbol === "-") {
-      minusNumbersArray.push(numberArrayStorage(numArray));
+      pendingNumbersArray.push(numArray.join(''));
+      pendingNumbersArray.push('-');
       numArray.length = 0;
-      $total.innerHTML = "";
+      $total.innerHTML = "0";
     }
     if (symbol === "x") {
-      timesNumbersArray.push(numberArrayStorage(numArray));
+      pendingNumbersArray.push(numArray.join(''));
+      pendingNumbersArray.push('*');
       numArray.length = 0;
-      $total.innerHTML = "";
+      $total.innerHTML = "0";
     }
     if (symbol === "/") {
-      divideNumbersArray.push(numberArrayStorage(numArray));
+      pendingNumbersArray.push(numArray.join(''));
+      pendingNumbersArray.push('/');
       numArray.length = 0;
-      $total.innerHTML = "";
+      $total.innerHTML = "0";
     }
     if (symbol === "=") {
-      if (addedNumbersArray.length > 0) {
-        finalNumber = parseInt(numArray.join(""), 10);
-        let addSum = 0;
-        for (let numbers of addedNumbersArray) {
-          addSum += add(numbers);
-        }
-        let finalSum = addSum + finalNumber;
-        $total.innerHTML = finalSum;
-      }
-      if (minusNumbersArray.length > 0) {
-        finalNumber = parseInt(numArray.join(""), 10);
-        let difference = minus(minusNumbersArray, finalNumber);
-        $total.innerHTML = difference;
-      }
-      if (timesNumbersArray.length > 0) {
-        finalNumber = parseInt(numArray.join(""), 10);
-        let product = times(timesNumbersArray, finalNumber);
-        $total.innerHTML = product;
-      }
-      if (divideNumbersArray.length > 0) {
-        finalNumber = parseInt(numArray.join(""), 10);
-        let quotient = divide(divideNumbersArray, finalNumber);
-        $total.innerHTML = quotient;
-      }
+      equals = eval(`${pendingNumbersArray.join(' ')} ${numArray.join('')}`);
+      $total.innerHTML = equals;
+      pendingNumbersArray.length = 0;
     }
   };
 
   const checkValue = value => {
     if (value === "C") {
       numArray.length = 0;
-      addedNumbersArray.length = 0;
-      minusNumbersArray.length = 0;
-      timesNumbersArray.length = 0;
-      divideNumbersArray.length = 0;
-      $total.innerHTML = "";
+      pendingNumbersArray.length = 0;
+      $total.innerHTML = "0";
     } else {
       collectNumbers(Number(value));
     }
@@ -86,61 +63,6 @@ const Calculate = (_ => {
   const collectNumbers = value => {
     numArray.push(value);
   };
-
-  const numberArrayStorage = nums => {
-    let firstNumber = nums.join("");
-    let addStoredToNumber = parseInt(firstNumber, 10);
-
-    return addStoredToNumber;
-  };
-
-  const add = numbersToAdd => {
-    let sum = 0;
-    sum += numbersToAdd;
-
-    return sum;
-  };
-
-  const minus = (numsArrayToSubtract, lastNumberBeforeEquals) => {
-    let difference = numsArrayToSubtract[0];
-
-    if (numsArrayToSubtract.length === 1) {
-      difference = difference;
-    } else {
-      for (let i = 1; i < numsArrayToSubtract.length; i++) {
-        difference -= numsArrayToSubtract[i];
-      }
-    }
-    return difference - lastNumberBeforeEquals;
-  };
-
-  const times = (numsArrayToMultipy, lastNumberBeforeEquals) => {
-    let product = numsArrayToMultipy[0];
-
-    if (numsArrayToMultipy.length === 1) {
-      product = product;
-    } else {
-      for (let i = 1; i < numsArrayToMultipy.length; i++) {
-        product *= numsArrayToMultipy[i];
-      }
-    }
-    return product * lastNumberBeforeEquals;
-  };
-
-  const divide = (numsArrayToDivide, lastNumberBeforeEquals) => {
-    let quotient = numsArrayToDivide[0];
-
-    if (numsArrayToDivide.length === 1) {
-      quotient = quotient;
-    } else {
-      for (let i = 1; i < numsArrayToDivide.length; i++) {
-        quotient /= numsArrayToDivide[i];
-      }
-    }
-    return quotient / lastNumberBeforeEquals;
-  };
-
-  const total = (a, b) => {};
 
   const init = _ => {
     listeners();
