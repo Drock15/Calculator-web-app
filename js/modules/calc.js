3b2cad41f58304b5b348a02b4a1fa0ad93f026db
Daniel Rock <rockdaniel87@yahoo.com>
2019-03-15 15:06:06 -0400

    Calculator functionality works & styled total

--- js/modules/calc.js
+++ js/modules/calc.js
@@ -1,67 +1,42 @@
 const Calculate = (_ => {
-  let numArray = [];
+  let lastNumberTotal;
   let pendingNumbersArray = [];
-  let equals;
 
-  const $calculator = document.querySelector(".container");
-  const $total = document.querySelector(".calculator__total");
+  // Cache the DOM
+  const calculatorTotalScreen = document.querySelector(".calculator__total");
+  const calculatorBtns = document.querySelector(".calculator-btn__container");
 
   const listeners = _ => {
-    $calculator.addEventListener("click", event => {
-      if (event.target.matches(".digits")) {
-        checkValue(event.target.innerHTML);
-        $total.innerHTML = numArray.join("");
-      }
-      if (event.target.matches(".operator")) {
-        checkOperator(event.target.innerHTML);
+    calculatorBtns.addEventListener("click", event => {
+      if (event.target.value === "C") {
+        pendingNumbersArray.length = 0;
+        calculatorTotalScreen.innerHTML = "0";
+      } else if (event.target.value === "=") {
+        calculatorTotalScreen.innerHTML = outputTotalScreen(
+          pendingNumbersArray
+        );
+        pendingNumbersArray.length = 0;
+        pendingNumbersArray.push(lastNumberTotal);
+      } else {
+        checkValue(event.target.value);
       }
     });
   };
 
-  const checkOperator = symbol => {
-    if (symbol === "+") {
-      pendingNumbersArray.push(numArray.join(''));
-      pendingNumbersArray.push('+');
-      numArray.length = 0;
-      $total.innerHTML = "0";
-    }
-    if (symbol === "-") {
-      pendingNumbersArray.push(numArray.join(''));
-      pendingNumbersArray.push('-');
-      numArray.length = 0;
-      $total.innerHTML = "0";
-    }
-    if (symbol === "x") {
-      pendingNumbersArray.push(numArray.join(''));
-      pendingNumbersArray.push('*');
-      numArray.length = 0;
-      $total.innerHTML = "0";
-    }
-    if (symbol === "/") {
-      pendingNumbersArray.push(numArray.join(''));
-      pendingNumbersArray.push('/');
-      numArray.length = 0;
-      $total.innerHTML = "0";
-    }
-    if (symbol === "=") {
-      equals = eval(`${pendingNumbersArray.join(' ')} ${numArray.join('')}`);
-      $total.innerHTML = equals;
-      pendingNumbersArray.length = 0;
-    }
-  };
-
   const checkValue = value => {
-    if (value === "C") {
-      numArray.length = 0;
-      pendingNumbersArray.length = 0;
-      $total.innerHTML = "0";
+    if (value !== "x") {
+      pendingNumbersArray.push(value);
     } else {
-      collectNumbers(Number(value));
+      pendingNumbersArray.push("*");
     }
+
+    calculatorTotalScreen.innerHTML = pendingNumbersArray.join("");
   };
 
-  const collectNumbers = value => {
-    numArray.push(value);
+  const outputTotalScreen = pendingCalculation => {
+    lastNumberTotal = eval(pendingCalculation.join(""));
+
+    return lastNumberTotal;
   };
 
   const init = _ => {
